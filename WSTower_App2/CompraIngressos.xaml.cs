c904a8842cadb84d1de5fec40227db9b16252175
renansalvino <renansalvinomartins454@gmail.com>
2020-07-17 09:24:55 -0300

    Interfaces de Detalhes e de Compra de Ingresso [CHECK]

---
+++ WSTower_App2/CompraIngressos.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WSTower_App2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CompraIngressos : ContentPage
+    {
+        public class CompraIngressoscs
+        {
+
+        }
+        public CompraIngressos()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
---
+++ WSTower_App2/DetalhesPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WSTower_App2
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DetalhesPage : ContentPage
+    {
+        public DetalhesPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_BindingContextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
